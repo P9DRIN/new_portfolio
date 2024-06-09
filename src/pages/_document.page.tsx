6b9bf6bd188c9258afcc362fbddd7658386d060a
Lima Silva <pedrohenriquelimasilva2837@hotmail.com>
2024-06-09 17:58:33 -0300

    fix: next script

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="pt-br" suppressHydrationWarning>
+    <Html lang="pt-BR" suppressHydrationWarning>
       <Head>
         <link rel="icon" href="/vercel.svg" type="image/x-icon" />
       </Head>
